f802bb6d86ca38876c2bc68d34a5985bd6d16033
Ubuntu <ubuntu@ip-172-31-26-88.us-west-1.compute.internal>
2024-11-25 20:06:40 +0000

    Add fullscreen mode

--- repo.sh
+++ repo.sh
@@ -9,5 +9,14 @@ export OMNI_REPO_ROOT="$( cd "$(dirname "$0")" ; pwd -P )"
 SCRIPT_DIR=$(dirname ${BASH_SOURCE})
 cd "$SCRIPT_DIR"
 
+if [[ "$1" == "package" ]]; then
+    if [ -d "${OMNI_REPO_ROOT}/modified" ]; then
+       cp -r "modified/sunstudy_webrtc"/* "_build/linux-x86_64/release/extscache"
+        echo "Extensions copied to _build/extscache."
+    else
+        echo "No 'modified' folder found, skipping extension copy."
+    fi
+fi
+
 # Use "exec" to ensure that envrionment variables don't accidentally affect other processes.
 exec "tools/packman/python.sh" tools/repoman/repoman.py "$@"
